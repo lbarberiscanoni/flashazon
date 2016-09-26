1e7179a20a751bbc860f4f6dfac77cc7ad3d1980
lbarberiscanoni <hllbck7@gmail.com>
2016-09-25 18:31:10 -0700

    1) added the specs to remmeber what info to look extract 2) switched to pdftotxt utility

--- pdfToTxt.py
+++ pdfToTxt.py
@@ -1,29 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import sys
 import os
 import time
+import subprocess
 
-pdfConverUrl = "http://pdf2html.tabesugi.net:8080"
 pwdPath = str(os.getcwd()) + "/"
 fileToConvert = str(sys.argv[1])
 
-browser = webdriver.Chrome()
+subprocess.call("pdftotext " + pwdPath + fileToConvert + ".pdf", shell=True)
 
-browser.get(pdfConverUrl)
+with open(fileToConvert + ".txt", "r") as file:
+    draft = file.read()
+    print draft
 
-fileInput = browser.find_element_by_name("f")
-fileInput.send_keys(pwdPath + fileToConvert)
-
-convertBtn = browser.find_element_by_xpath("//input[@name='c' and @value='Convert to TEXT']")
-convertBtn.click()
-
-time.sleep(2)
-
-result = browser.find_element_by_tag_name("pre")
-result = result.text
-result = result.split("\n")[2]
-
-print result
-
-browser.close()
